650f3b1e6dce49f3eccd38eee1a549b9aae46088
abelsiqueira <abelsiqueira@users.noreply.github.com>
2022-03-08 23:27:11 +0000

    Deploying to gh-preview from  @ 98b3a2637be5fa67072491951114d697431d5c2a 🚀

---
+++ previews/PR5/assets/index/code/ex11.jl
@@ -0,0 +1,2 @@
+# This file was generated, do not modify it. # hide
+Jx = jac_op_residual(model,x)
---
+++ previews/PR5/assets/index/code/ex5.jl
@@ -0,0 +1,4 @@
+# This file was generated, do not modify it. # hide
+paths = fetch_ba_group("ladybug")
+println(length(paths))
+println(paths[1])
